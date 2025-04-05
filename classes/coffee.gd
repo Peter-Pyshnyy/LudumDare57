919640e5b13b9da3f5f368fc5db19a1ed9d83718
antonyyyyyyyantonyyyyyyy <129731460+antonyyyyyyy@users.noreply.github.com>
2025-04-05 23:53:59 +0200

    Improved coffee and skateboard

--- classes/coffee.gd
+++ classes/coffee.gd
@@ -4,8 +4,14 @@ var spilled: bool = false
 
 signal spilled_signal
 
+@onready var gpu_particles_2d: GPUParticles2D = $GPUParticles2D
+
 func _physics_process(delta: float) -> void:
-	if transform.get_rotation() > 70 || transform.get_rotation() < -70:
+	print("coffee rot: ", transform.get_rotation())
+	if transform.get_rotation() > deg_to_rad(70) || transform.get_rotation() < deg_to_rad(-70):
+		gpu_particles_2d.emitting = true
 		if !spilled:
 			spilled = true
 			spilled_signal.emit()
+	else:
+		gpu_particles_2d.emitting = false
