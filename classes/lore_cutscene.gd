6e8bcc52cb269482aa020f85eb58072a827d2ab9
Matetiga <martinnajera@me.com>
2025-04-08 00:20:11 +0200

    skip cutscene and reload key added

--- classes/lore_cutscene.gd
+++ classes/lore_cutscene.gd
@@ -9,7 +9,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseButton:
+		get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 func _on_video_stream_player_finished() -> void:
 	get_tree().change_scene_to_file("res://scenes/game.tscn")
