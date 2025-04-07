8542c76037838194c62a45784d027130041912d8
antonyyyyyyyantonyyyyyyy <129731460+antonyyyyyyy@users.noreply.github.com>
2025-04-07 20:49:09 +0200

    cutablke seam und so

--- classes/cutable.gd
+++ classes/cutable.gd
@@ -5,6 +5,7 @@ var last_time: int
 
 var lifes: int = 10
 
+@onready var seam_uncut: Sprite2D = $SeamUncut
 @onready var seam_halfcut: Sprite2D = $SeamHalfcut
 @onready var seam_cut: Sprite2D = $SeamCut
 
@@ -18,8 +19,10 @@ func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_inde
 			print("reduce life, ", lifes)
 			if lifes < 8:
 				seam_halfcut.visible = true
+				seam_uncut.hide()
 			if lifes <= 0:
 				seam_cut.show()
+				seam_halfcut.hide()
 				process_mode = Node.PROCESS_MODE_DISABLED
 		
 		last_time = Time.get_ticks_msec()
